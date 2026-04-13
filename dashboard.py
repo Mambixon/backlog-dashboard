f5415a7ddebbf1465b8c59a91e5389015c5305a4
Mambixon <czarna.mamba102@gmail.com>
2026-04-13 17:07:32 +0100

    Add add_case function

--- dashboard.py
+++ dashboard.py
@@ -14,5 +14,31 @@ def view_cases():
     print( 8*"=", "TM BACKLOG DASHBOARD", 8*"=")
 
     for case in cases:
-        print(case["id"],case["company name"],case["status"],case["owner"], case["priority"])
-view_cases()
+        print(case["id"],case["transaction_id"],case["company name"],case["status"],case["owner"], case["priority"])
+
+
+def add_case():
+
+    with open("backlog.csv","r") as file:
+        reader = csv.DictReader(file)
+        cases = list(reader)
+
+    transaction_id = input("Enter transaction id: ")
+    company_name = input("Enter company name: ")
+    root_cause = input("Enter company root cause: ")
+    status = input("Enter status: ")
+    owner = input("Enter owner: ")
+    priority = input("Enter priority: ")
+    comment = input("Enter comment: ")
+    new_id = str(int(cases[-1]["id"]) + 1).zfill(3)
+
+    new_case = {"id": new_id, "transaction_id": transaction_id, "company name": company_name, "root_cause": root_cause, "status": status, "owner": owner,"priority":priority, "comment":comment}
+
+    with open("backlog.csv","a", newline = "") as file:
+        writer = csv.DictWriter(file, fieldnames =  cases[0].keys())
+        writer.writerow(new_case)
+
+
+
+view_cases()       
+add_case()
